40f8cbd314f7a0778325e685f9c2f720bcc989ce
DD-459-1030 <1.kouki.baba.20@gmail.com>
2022-08-16 15:22:56 +0900

    name changed

---
+++ for_GCC/dirDisCalc.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+void directionCalc(double lat1,double lng1,double lat2,double lng2,double *dis,double *dir){
+	double pi = 3.14159265359;
+	lat1=lat1*pi/180;
+	lat2=lat2*pi/180;
+	lng1=lng1*pi/180;
+	lng2=lng2*pi/180;
+	double earth_radius=6378.137;
+	*dis = earth_radius * acos(sin(lat1)*sin(lat2)+cos(lat1)*cos(lat2)*cos(lng2-lng1));
+	*dir = 90-(180/pi*(atan2((cos(lat1)*tan(lat2)-sin(lat1)*cos(lng2-lng1)),sin(lng2-lng1))));
+
+}
+
+int main(void){
+	double dis,dir;
+
+	directionCalc(245.423545,140,34,135,&dis,&dir);
+	printf("distance:%lf,direction:%lf\n",dis,dir);
+	return 0;
+}
