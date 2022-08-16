c9874fe88521338b5f0381bfb574b0b740bfcbd9
DD-459-1030 <1.kouki.baba.20@gmail.com>
2022-08-16 16:42:53 +0900

    ok

--- for_GCC/moveDirCalc.c
+++ for_GCC/moveDirCalc.c
@@ -22,7 +22,7 @@ void moveDirCalc(double direction,double compass_point,double *move_direction){
 }
 int main(void){
 	double move_direction;
-	move_DirCalc(245.423545,0.0,&move_direction);
+	moveDirCalc(245.423545,0.0,&move_direction);
 	printf("%lf",move_direction);
 	return 0;
 }
