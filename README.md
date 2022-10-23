66a46882def4d0b224c280cd0eff9640ef0b3090
shushutunn <asapy0307@gmail.com>
2022-10-23 17:38:33 +0900

    refector: Write Readme, split contest code

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # GPSQZSSRobotCar_NITNC
-hogehoge
+
+GNSS・QZSS ロボットカーコンテスト 2022
+エントリー No.12 　奈良工業高等専門学校　ロボット名：無限廻車
+**祝・特別賞受賞！！**
+
+1. ディレクトリについて
+   |:--------------|:-----------------------------------|
+   | compassdata | 地磁気センサ補正用のデータ |
+   | ContestCode | **大会本番で用いたソースコード** |
+   | for_ArduinoDue| ArduinoDue での検証のプログラム |
+   | for_GCC | GCC での検証プログラム |
+
+2.主な検証プログラム
++for_ArduinoDue 内
++getdata_func.ino: 地磁気センサ、GNSS データ取得の検証
++ApproachWP.ino: 1 つの地点に近づく検証
++FigureEight.ino: 8 の字周回検証
++BonusPoint.ino: ボーナスポイント検証
++reiwa.ino: REIWA ポイント検証バージョン１
++reiwa2.ino: REIWA ポイント検証バージョン２
++reiwa2_TimeMemory.ino: リスタート対応のため、スタート時刻を EEPROM に記録する機能を追加。大会本番のソースコード。
+
+3.ハードウェア構成
--- for_ArduinoDue/24LC256/24LC256.ino
+++ for_ArduinoDue/24LC256/24LC256.ino
@@ -1,43 +1,44 @@
-#include <Wire.h>    
- 
-#define disk1 0x50    //Address of 24LC256 eeprom chip
- 
+#include <Wire.h>
+
+#define disk1 0x50 // Address of 24LC256 eeprom chip
+
 void setup(void)
 {
-  Serial.begin(9600);
-  Wire.begin();  
- 
-  unsigned int address = 0;
-  Serial.println(readEEPROM(disk1, address), DEC); 
-  writeEEPROM(disk1, address, 45 );
-  Serial.println(readEEPROM(disk1, address), DEC);
+	Serial.begin(9600);
+	Wire.begin();
+
+	unsigned int address = 0;
+	// Serial.println(readEEPROM(disk1, address), DEC);
+	writeEEPROM(disk1, address, readEEPROM(disk1, address) + 1);
+	Serial.println(readEEPROM(disk1, address), DEC);
 }
- 
-void loop(){}
- 
-void writeEEPROM(int deviceaddress, unsigned int eeaddress, byte data ) 
+
+void loop() {}
+
+void writeEEPROM(int deviceaddress, unsigned int eeaddress, byte data)
 {
-  Wire.beginTransmission(deviceaddress);
-  Wire.write((int)(eeaddress >> 8));   // MSB
-  Wire.write((int)(eeaddress & 0xFF)); // LSB
-  Wire.write(data);
-  Wire.endTransmission();
- 
-  delay(5);
+	Wire.beginTransmission(deviceaddress);
+	Wire.write((int)(eeaddress >> 8));	 // MSB
+	Wire.write((int)(eeaddress & 0xFF)); // LSB
+	Wire.write(data);
+	Wire.endTransmission();
+
+	delay(5);
 }
- 
-byte readEEPROM(int deviceaddress, unsigned int eeaddress ) 
+
+byte readEEPROM(int deviceaddress, unsigned int eeaddress)
 {
-  byte rdata = 0xFF;
- 
-  Wire.beginTransmission(deviceaddress);
-  Wire.write((int)(eeaddress >> 8));   // MSB
-  Wire.write((int)(eeaddress & 0xFF)); // LSB
-  Wire.endTransmission();
- 
-  Wire.requestFrom(deviceaddress,1);
- 
-  if (Wire.available()) rdata = Wire.read();
- 
-  return rdata;
+	byte rdata = 0xFF;
+
+	Wire.beginTransmission(deviceaddress);
+	Wire.write((int)(eeaddress >> 8));	 // MSB
+	Wire.write((int)(eeaddress & 0xFF)); // LSB
+	Wire.endTransmission();
+
+	Wire.requestFrom(deviceaddress, 1);
+
+	if (Wire.available())
+		rdata = Wire.read();
+
+	return rdata;
 }
